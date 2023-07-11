54a2c0b23b6c174f3eb056f52a561cfaa1b3a77f
hasanhttps <hasanabdullazad@gmail.com>
2023-07-11 21:55:08 +0400

    Latest Version 1.0

---
+++ WPF Bind Map Lesson/ViewModels/MainViewModel.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.Maps.MapControl.WPF;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using WPF_Bind_Map_Lesson.Commands;
+using WPF_Bind_Map_Lesson.Models;
+
+namespace WPF_Bind_Map_Lesson.ViewModels {
+    public class MainViewModel : INotifyPropertyChanged {
+
+        // Private Fields
+
+        private Bakubus? myBuses;
+        private List<string> BusNames = new();
+        private List<Pushpin> pushpins = new();
+        private ComboBox SearchFilterBus;
+        private Map Map;
+        private int busCount = 233;
+
+        // Properties
+
+        public ICommand? SearchCommand { get; set; }
+
+        public Bakubus? MyBuses { 
+            get => myBuses; 
+            set {
+                myBuses = value;
+                INotifyPropertyChanged();
+            } 
+        }
+        public int BusCount { get => busCount;
+            set {
+                busCount = value;
+                INotifyPropertyChanged();
+            }
+        }
+        public Attributes Bus { get; set; }
+
+        // Constructors
+
+        public MainViewModel() { }
+        public MainViewModel(List<string> busNames, List<Pushpin> pushPins, Bakubus bakubuses, ComboBox searchFilterBus, Map map, Attributes attributes) {
+
+            SearchFilterBus = searchFilterBus;
+            myBuses = bakubuses;
+            BusNames = busNames;
+            pushpins = pushPins;
+            Map = map;
+            Bus = attributes;
+            SearchCommand = new RelayCommand(SearchButtonCommand);
+        }
+
+        // Command Functions
+
+        private void SearchButtonCommand(object? param) {
+            BusCount = 0;
+            Map.Children.Clear();
+            string routecode = (SearchFilterBus.SelectedItem as string)!;
+            if (routecode == "All") {
+                foreach (var pushpin in pushpins) {
+                    Map.Children.Add(pushpin);
+                    BusCount++;
+                }
+                return; 
+            }
+            foreach(var busname in BusNames) {
+                if (busname == routecode)
+                    foreach (var pushpin in pushpins) {
+                        if (pushpin.Content.ToString() == routecode) {
+                            Map.Children.Add(pushpin);
+                            BusCount++;
+                        }
+                    }
+            }
+
+        }
+
+        // INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        void INotifyPropertyChanged([CallerMemberName] string? name = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
---
+++ WPF Bind Map Lesson/Views/MainView.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Windows;
+using System.Net.Http;
+using System.Text.Json;
+using System.Windows.Media;
+using System.ComponentModel;
+using WPF_Bind_Map_Lesson.Models;
+using System.Collections.Generic;
+using Microsoft.Maps.MapControl.WPF;
+using WPF_Bind_Map_Lesson.ViewModels;
+using WPF_Bind_Map_Lesson.Commands;
+using System.Runtime.CompilerServices;
+using System.Linq;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+ 
+
+namespace WPF_Bind_Map_Lesson.Views {
+    public partial class MainView : Window, INotifyPropertyChanged {
+
+        private Bakubus? myBuses;
+        private List<string> BusNames = new();
+        public Attributes Bus { get; set; } = new();
+        public Bakubus? MyBuses { 
+            get => myBuses; 
+            set {
+                myBuses = value;
+                INotifyPropertyChanged();
+            } 
+        }
+        private List<Pushpin> pushpins = new();
+
+ 
+
+        public MainView() {
+            InitializeComponent();
+            DataContext = new MainViewModel(BusNames, pushpins, MyBuses, SearchFilterBus, Map, Bus);
+        }
+
+ 
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e) {
+            var client = new HttpClient();
+            var jsonStr = await client.GetStringAsync("https://raw.githubusercontent.com/CavidAtamoghlanov/newBakuBus/main/BakuBusApi");
+
+            MyBuses = JsonSerializer.Deserialize<Bakubus>(jsonStr);
+            BusNames.Add("All");
+
+ 
+
+            foreach(var bus in MyBuses!.BUS) {
+                if (!BusNames.Contains(bus.attributes.DISPLAY_ROUTE_CODE))
+                    BusNames.Add(bus.attributes.DISPLAY_ROUTE_CODE);
+
+                Pushpin pushpin = new Pushpin();
+                pushpin.Content = bus.attributes.DISPLAY_ROUTE_CODE;
+                pushpin.Tag = bus.attributes;
+                pushpin.MouseEnter += PushPinMouseHover;
+                pushpin.ToolTip = new();
+
+                Location location = new Location(Convert.ToDouble(bus.attributes.LATITUDE), Convert.ToDouble(bus.attributes.LONGITUDE));
+                pushpin.Location = location;
+
+ 
+
+                pushpins.Add(pushpin);
+                Map.Children.Add(pushpin);
+            }
+            SearchFilterBus.ItemsSource = BusNames;
+
+ 
+
+            foreach(var busname in BusNames) {
+                SolidColorBrush colorBrush = new SolidColorBrush() {
+                    Color = Color.FromRgb((byte)Random.Shared.Next(0, 255), (byte)Random.Shared.Next(0, 255), (byte)Random.Shared.Next(0, 255))
+                };
+                pushpins.ForEach((Pushpin) => {
+                    if (busname == Pushpin.Content as string) Pushpin.Background = colorBrush;
+                });
+            }
+        }
+
+ 
+
+        private void PushPinMouseHover(object sender, MouseEventArgs e) {
+            Attributes attributes = ((sender as Pushpin).Tag as Attributes)!;
+            Bus.BUS_ID = attributes.BUS_ID;
+            Bus.PREV_STOP = attributes.PREV_STOP;
+            Bus.CURRENT_STOP = attributes.CURRENT_STOP;
+            Bus.DISPLAY_ROUTE_CODE = attributes.DISPLAY_ROUTE_CODE;
+            Bus.ROUTE_NAME = attributes.ROUTE_NAME;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        void INotifyPropertyChanged([CallerMemberName] string? name = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
